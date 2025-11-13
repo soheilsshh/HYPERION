1e06dfe3beb8ef2d8799b31cc507c2d7871bfa65
soheilsshh <soheilswhh@gmail.com>
2025-11-13 15:46:36 +0330

    add urls and some templates

--- monitor/views.py
+++ monitor/views.py
@@ -1,7 +1,12 @@
-# monitoring/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .forms import ServiceForm
 from .models import Service
+
+
+def service_list(request):
+    services = Service.objects.order_by('-last_checked')
+    return render(request, 'service_list.html', {'services': services})
 
 
 def add_service(request):
@@ -11,12 +16,27 @@ def add_service(request):
             service = form.save(commit=False)
             service.user = request.user
             service.save()
-            return redirect('service_list')
+            return redirect(service_list)
     else:
         form = ServiceForm()
-    return render(request, 'monitoring/add_service.html', {'form': form})
+    return render(request, 'add_service.html', {'form': form})
 
 
-def service_list(request):
-    services = Service.objects.filter(user=request.user)
-    return render(request, 'monitoring/list.html', {'services': services})
+def edit_service(request, pk):
+    service = get_object_or_404(Service, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, instance=service)
+        if form.is_valid():
+            form.save()
+            return redirect(service_list)
+    else:
+        form = ServiceForm(instance=service)
+    return render(request, 'add_service.html', {'form': form, 'edit': True})
+
+
+def delete_service(request, pk):
+    service = get_object_or_404(Service, pk=pk, user=request.user)
+    if request.method == 'POST':
+        service.delete()
+        return redirect(service_list)
+    return render(request, 'delete_service.html', {'service': service})
