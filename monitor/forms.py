0033d285d946cce8dbae8144611165a4b90c2236
soheilsshh <soheilswhh@gmail.com>
2025-11-16 20:06:30 +0330

    update form.py

--- monitor/forms.py
+++ monitor/forms.py
@@ -4,7 +4,16 @@ from .models import Service, UserAlert
 class ServiceForm(forms.ModelForm):
     class Meta:
         model = Service
-        fields = ['name', 'url', 'interval', 'is_active']
+        fields = [
+            'name',
+            'url',
+            'interval',
+            'is_active',
+            'db_username',
+            'db_password',
+            'db_database',   
+        ]
         widgets = {
             'interval': forms.NumberInput(attrs={'min': 1}),
+            'db_password': forms.PasswordInput(render_value=True),
         }
